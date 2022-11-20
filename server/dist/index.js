0b4f7f075fb800430cb3e70f11f2dc40d5ebeceb
cratas <petr.kratochvil.st1@vsb.cz>
2022-11-20 17:29:21 +0100

    orm settings done, next step is set auth

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,56 @@
-console.log("hello word");
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ormConnection = void 0;
+require("reflect-metadata");
+const typeorm_1 = require("typeorm");
+const express_1 = __importDefault(require("express"));
+const type_graphql_1 = require("type-graphql");
+const apollo_server_express_1 = require("apollo-server-express");
+const user_1 = require("./resolvers/user");
+const User_1 = require("./entities/User");
+exports.ormConnection = new typeorm_1.DataSource({
+    type: "postgres",
+    database: "keydestroyer",
+    username: "postgres",
+    password: "Admin123",
+    logging: true,
+    synchronize: true,
+    entities: [User_1.User],
+});
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    exports.ormConnection.initialize();
+    const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield (0, type_graphql_1.buildSchema)({
+            resolvers: [user_1.UserResolver],
+            validate: false,
+        }),
+        context: ({ req, res }) => ({
+            req,
+            res,
+        }),
+    });
+    yield apolloServer.start();
+    apolloServer.applyMiddleware({
+        app,
+    });
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    });
+});
+main().catch((err) => {
+    console.error(err);
+});
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,48 @@
-console.log("hello word")
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
+import { UserResolver } from "./resolvers/user";
+import { User } from "./entities/User";
+
+export const ormConnection = new DataSource({
+  type: "postgres",
+  database: "keydestroyer",
+  username: "postgres",
+  password: "Admin123",
+  logging: true,
+  synchronize: true,
+  entities: [User],
+});
+
+const main = async () => {
+  // init type orm connection
+  ormConnection.initialize();
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+    }), // passing objects, something like props
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({
+    app,
+    // cors: false,
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
